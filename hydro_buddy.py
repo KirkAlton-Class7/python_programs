4ff89e82671930deb8d1547dab7a72b2c64f02d0
KirkAlton-Class7 <232495916+KirkAlton-Class7@users.noreply.github.com>
2025-10-10 09:10:55 -0500

    updated .gitignore and hydro_buddy.py

--- hydro_buddy.py
+++ hydro_buddy.py
@@ -19,8 +19,6 @@
 #    - If they met or exceeded their goal, congratulate them for great hydration.
 # 5. End with a positive message to motivate them to keep going.
 
-import sys
-
 # Greeting
 name = input("Hi, I'm HydroBuddy! What's your name? ")
 print(f"\nHi {name}!")
@@ -46,24 +44,21 @@ while True:
         intake = int(intake)
         break # exit loop when valid
 
-# Special case: one ounce to reach goal
-leftover = goal - intake
-while leftover == 1:
-    print(f"\nYou're right there, {name}! Only one ounce of water left to reach your goal!")
-    sys.exit()
-
-# All other cases (percentage towards goal)
+# Motivation (based on percentage towards goal)
 if intake >= goal:
-    print(f"\nGreat job {name}, you met your goal of {goal} ounces of water!")
+    print(f"\nGreat job {name}! You met your goal of {goal} ounces of water!")
+# Special case:
+if goal - intake == 1:
+    print(f"\nYou're right there, {name}! Only one ounce of water left to reach your goal!")
 elif intake >= goal*.7:
-    print(f"\nKeep going. You're almost there, {name}! Only {goal - intake} ounces of water left to reach your goal!")
+    print(f"\nKeep going. You're almost there {name}! Only {goal - intake} ounces of water left to reach your goal!")
 elif intake >= goal*.6:
     print(f"\nGood job {name}. Only {goal - intake} ounces of water left to reach your goal!")
 elif intake == goal*.5:
     print(f"\nYou're halfway there {name}! Only {goal - intake} ounces of water to go!")
 # Special case: no water intake
 elif intake == 0:
-    print(f"\nLet's get going, {name}. Drink some water!")
+    print(f"\nLet's get going {name}. Drink some water!")
 else:
     print(f"\nI know you can do it {name}! Keep drinking so you can reach your goal of {goal} ounces of water.")
 
@@ -75,3 +70,12 @@ else:
 # import sys then use sys.exit() to stop the entire program immediately.
 # while True loops run until break or sys.exit() is executed.
 # order of conditions matters — Python checks from top to bottom.
+
+# IMPROVEMENTS TO MAKE
+# 1. I think you commented out the if goal.isdigit() line in the goal input loop.
+# 2. what if the intake input is anything but a positive integer? Also why wouldn’t a float be acceptable input?
+# 3. Concerning best practices: you don’t need the final while loop, why not add this output to the if-else statement?
+# 4. This would also allow you to avoid using the sys.exit function and module entirely (which isn’t really a good practice).
+# 5. Take it to the next level and break it up into functions that return the user input.
+# 6. You already have what should be a function identified based on the comments
+# 7. Incorporate unit tests to test your code.
