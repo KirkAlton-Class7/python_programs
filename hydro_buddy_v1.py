97bcd54f804711c66de69c8b17e33e2a335ec738
KirkAlton-Class7 <232495916+KirkAlton-Class7@users.noreply.github.com>
2025-11-28 15:37:28 -0600

    updated via CLI 2025-11-28 15:37:28 CST

--- hydro_buddy_v1.py
+++ hydro_buddy_v1.py
@@ -25,11 +25,14 @@
 
 # Greeting
 name = input("Hi, I'm HydroBuddy! What's your name? ")
-print(f"\nHi {name}!")
+def greet(name):
+    print(f"\nHi {name}!")
 
 # Input Goal
+
 while True:
     goal = input(f"\nWhat's your hydration goal? (Enter number of ounces as a whole number) " )
+
     # Special case: goal = 0
     if goal == "0":
         print(f"\nCome on {name}, you can do better than that!")
@@ -50,15 +53,15 @@ while True:
 
 # Motivation (based on percentage towards goal)
 if intake >= goal:
-    print(f"\nGreat job {name}! You met your goal of {goal} ounces of water!")
+    print(f"\nCongratulations {name}! You met your goal of {goal} ounces of water!")
 # Special case: one unit until goal
 if goal - intake == 1:
-    print(f"\nYou're right there, {name}! Only one ounce of water left to reach your goal!")
-elif intake >= goal*.7:
-    print(f"\nKeep going. You're almost there {name}! Only {goal - intake} ounces of water left to reach your goal!")
-elif intake >= goal*.6:
+    print(f"\nYou're right there {name}! Only one ounce of water left to reach your goal!")
+elif intake >= goal*.7 and intake < goal:
+    print(f"\nYou're almost there {name}! Only {goal - intake} ounces of water left to reach your goal!")
+elif intake >= goal*.6 and intake < goal:
     print(f"\nGood job {name}. Only {goal - intake} ounces of water left to reach your goal!")
-elif intake == goal*.5:
+elif intake == goal*.5 and intake < goal:
     print(f"\nYou're halfway there {name}! Only {goal - intake} ounces of water to go!")
 # Special case: no water intake
 elif intake == 0:
@@ -67,6 +70,7 @@ else:
     print(f"\nI know you can do it {name}! Keep drinking so you can reach your goal of {goal} ounces of water.")
 
 # WHAT I LEARNED
+# Keep your code DRY. DON'T REPEAT YOURSELF!
 # Convert digits with string data types to integers with int()
 # .isdigit() checks if input contains only digits before converting.
 # use break to exit a loop completely.
